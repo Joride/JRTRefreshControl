09a48f1e2c99235b9c6614ba13b9c20673f0eb32
Joride <kerrelinc@gmail.com>
2015-10-01 20:39:29 +0200

    Initial Commit

---
+++ JRTRefreshControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JRTRefreshControl
+//
+//  Created by Jorrit van Asselt on 01-10-15.
+//  Copyright © 2015 Kerrelinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JRTRefreshControl/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  JRTRefreshControl
+//
+//  Created by Jorrit van Asselt on 01-10-15.
+//  Copyright © 2015 Kerrelinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
